b4b8792f8ff0b1cef18d2ed8ca4673952165ac73
changfei <tianwening@163.com>
2021-07-27 18:03:05 +0800

    研究vuepress相关配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,21 @@
+const path = require('path');
 module.exports = {
-  title: '个人学习使用',
+  title: '天问ing',
   description: '这里是一段简单的描述信息',
+  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
   base: '/docs/', // 部署时使用的真实路径
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@images': './public/images',
-      },
-    },
+  themeConfig: {
+    nav: [
+      // 导航栏配置
+      { text: 'vuex', link: '/vuex/' },
+      { text: '百度', link: 'https://baidu.com' },
+    ],
   },
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       '@alias': path.resolve(__dirname, './public/images/'),
+  //     },
+  //   },
+  // },
 };
